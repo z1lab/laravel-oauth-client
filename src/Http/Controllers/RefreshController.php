5a8a1ce9455e42ca65ab28fee3294f7046c6e33e
Olimar Ferraz <olimarferraz@gmail.com>
2018-11-07 18:02:09 -0200

    Added User Resource to refresh request;

--- src/Http/Controllers/RefreshController.php
+++ src/Http/Controllers/RefreshController.php
@@ -10,11 +10,16 @@ namespace Z1lab\OpenID\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Cookie;
 use Z1lab\OpenID\Client;
+use Z1lab\OpenID\Http\Resources\User;
+use Z1lab\OpenID\Traits\CreateUserTrait;
+use Z1lab\OpenID\Traits\ValidateTokenTrait;
 
 class RefreshController
 {
+    use CreateUserTrait, ValidateTokenTrait;
     /**
      * @param Request $request
      * @return JsonResponse
@@ -23,7 +28,7 @@ class RefreshController
     {
         $refresh_token = Cookie::get(Client::$refresh_cookie, '');
 
-        if (empty($refresh_token)) return new JsonResponse(['success' => FALSE], 401);
+        if (empty($refresh_token)) return new JsonResponse(['success' => FALSE], Response::HTTP_UNAUTHORIZED);
 
         $scopes = 'openid' . $request->filled('scopes') ? (' ' . $request->get('scopes')) : '';
         $client = new \GuzzleHttp\Client(['base_uri' => config('openid.server')]);
@@ -43,7 +48,11 @@ class RefreshController
             $auth_expires = $result->expires_in / 60;
             $refresh_expires = $result->refresh_expires_in / 60;
 
-            return (new JsonResponse(['success' => TRUE]))
+            $user = $this->createUserFromToken($this->validateToken($result->id_token));
+
+            $data = collect($user->toArray());
+
+            return (new User($data))->response()
                 ->withCookie(
                     Cookie::make(
                         Client::$access_cookie,
@@ -66,7 +75,7 @@ class RefreshController
                     )
                 );
         } catch (\Exception $e) {
-            return new JsonResponse([], 401);
+            return new JsonResponse($e->getMessage(), Response::HTTP_UNAUTHORIZED);
         }
     }
 }
