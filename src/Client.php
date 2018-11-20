395c03c641b7d27b8005c1df31664147bdba2be4
Felipe da Silva Pinheiro <felipe.spinheiro88@gmail.com>
2018-11-20 11:36:41 -0200

    Remove refresh from client token

--- src/Client.php
+++ src/Client.php
@@ -52,9 +52,6 @@ class Client
         if ($client_token = Cache::get('client_token', NULL))
             return $client_token;
 
-        if ($refresh_client_token = Cache::get('client_refresh_token', NULL))
-            return $this->refreshClientToken($refresh_client_token, $scopes);
-
         $client = new \GuzzleHttp\Client(['base_uri' => config('openid.server')]);
 
         try {
@@ -70,51 +67,9 @@ class Client
             return NULL;
         }
 
-        return $this->interpretBody($response->getBody());
-    }
+        $result = json_decode($response->getBody());
 
-    /**
-     * @param string $client_refresh_token
-     * @param array $scopes
-     * @return null|string
-     */
-    private function refreshClientToken(string $client_refresh_token, array $scopes = [])
-    {
-        $client = new \GuzzleHttp\Client(['base_uri' => config('openid.server')]);
-
-        try {
-            $response = $client->post('oauth/token', [
-                'form_params' => [
-                    'grant_type'    => 'refresh_token',
-                    'refresh_token' => $client_refresh_token,
-                    'client_id'     => config('openid.client.id'),
-                    'client_secret' => config('openid.client.secret'),
-                    'scope'         => empty($scopes) ? '' : implode(' ', $scopes),
-                ],
-            ]);
-        } catch (\Exception $e) {
-            return NULL;
-        }
-
-        return $this->interpretBody($response->getBody());
-    }
-
-    /**
-     * @param string $body
-     * @return string
-     */
-    private function interpretBody(string $body)
-    {
-        $result = json_decode($body);
-
-        $auth_expires = $result->expires_in / 60;
-        $refresh_expires = $result->refresh_expires_in / 60;
-
-        Cache::remember('client_refresh_token', $refresh_expires, function () use ($result) {
-            return $result->refresh_token;
-        });
-
-        return Cache::remember('client_token', $auth_expires, function () use ($result) {
+        return Cache::remember('client_token', $result->expires_in / 60, function () use ($result) {
             return $result->access_token;
         });
     }
